f984e311efe04e6426a94d240637e5fd3fa2c73c
Kamil Czech <33509305+k-czech@users.noreply.github.com>
2021-04-29 22:25:01 +0200

    added starter configuration app (#1)

---
+++ src/assets/styles/theme.js
@@ -0,0 +1,14 @@
+export const theme = {
+  colors: {
+    black: '#000',
+    white: '#fff',
+  },
+  fontSize: {
+    xxl: '34px',
+    xl: '24px',
+    l: '16px',
+    m: '12px',
+    s: '11px',
+  },
+  fontFamily: `'Lato', sans-serif`,
+};
---
+++ src/components/templates/MainTemplate/MainTemplate.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Wrapper } from 'components/templates/MainTemplate/MainTemplate.styles';
+
+const MainTemplate = ({ children }) => {
+  return <Wrapper>{children}</Wrapper>;
+};
+
+export default MainTemplate;
